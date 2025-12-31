a4a91e2aba452c7fa882704fbaf0c372fb0dac47
nisha <2016ume1288@mnit.ac.in>
2025-12-31 08:48:09 +0530

    create convos and messages schema

--- alembic/env.py
+++ alembic/env.py
@@ -4,8 +4,10 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from sqlmodel import SQLModel
 
 from backend.config.settings import config_settings
+from backend.schema.schema import Conversations, Messages
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ backend/schema/schema.py
@@ -0,0 +1,70 @@
+from typing import Optional, List
+from datetime import datetime
+from uuid import UUID
+from sqlmodel import SQLModel, Field, Relationship
+from sqlalchemy import Column, DateTime, Text, func, Enum, UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+
+from uuid6 import uuid7
+
+class Conversations(SQLModel, table=True):
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    
+    session_id: UUID = Field(
+        sa_column=Column(
+            PG_UUID(as_uuid=True),
+            nullable=False,
+            unique=True,
+            index=True,
+        ),
+        default_factory=uuid7,
+    )
+
+    created_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            nullable=False,
+            server_default=func.now(), 
+        )
+    )
+
+    messages: List["Messages"] = Relationship(
+        back_populates="conversation",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    )
+
+class Messages(SQLModel, table=True):
+   
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    conversation_id: int = Field(
+        foreign_key="conversations.id",
+        index=True,
+        nullable=False,
+    )
+
+    sender: str = Field(
+        sa_column=Column(
+            Enum("user", "ai", name="message_sender"),
+            nullable=False,
+        )
+    )
+
+    text: str = Field(
+        sa_column=Column(Text, nullable=False)
+    )
+
+    created_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            nullable=False,
+            server_default=func.now(),
+        )
+    )
+
+    conversation: Optional[Conversations] = Relationship(back_populates="messages")
+
+
+
